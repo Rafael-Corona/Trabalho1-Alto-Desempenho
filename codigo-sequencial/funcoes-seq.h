5eba54457d5f7e3a3cb87c47a831a87832cf0282
Rafael-Corona <rafaelcorona2@gmail.com>
2022-10-23 17:37:43 -0300

    Reestruturação do projeto e correção de overflow de variável

---
+++ codigo-sequencial/funcoes-seq.h
@@ -0,0 +1,301 @@
+#define NOTAS_POSSIVEIS 101
+#define NOME_ARQ_ENTRADA "entrada.txt"
+
+#ifdef DEBUG
+unsigned teste[72] = {
+    30, 40, 20, 80, 85, 10,
+    10, 20, 30, 40, 50, 60,
+    60, 50, 40, 30, 20, 10,
+    70, 55, 35, 80, 95, 27,
+    35, 45, 25, 85, 90, 15,
+    15, 25, 35, 45, 55, 65,
+    65, 55, 45, 35, 25, 15,
+    75, 60, 40, 85, 100, 32,
+    20, 30, 10, 70, 75, 00,
+    00, 10, 20, 30, 40, 50,
+    50, 40, 30, 20, 10, 00,
+    60, 45, 25, 70, 85, 17
+};
+#endif
+
+void atribuir_parametros_entrada(unsigned *total_regioes, unsigned *cidades_por_regiao, unsigned *alunos_por_cidade, unsigned *semente)
+{
+
+    #ifdef DEBUG
+    *total_regioes = 3;
+    *cidades_por_regiao = 4;
+    *alunos_por_cidade = 6;
+    *semente = 7;
+    #else
+    FILE *fp = fopen(NOME_ARQ_ENTRADA, "r");
+
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir arquivo com as informações de entrada!\n");
+        fprintf(stderr, "Verifique o arquivo de nome \"entrada.txt\"\n");
+        exit(-1);
+    }
+
+    fscanf(fp, "%d", total_regioes);
+    fscanf(fp, "%d", cidades_por_regiao);
+    fscanf(fp, "%d", alunos_por_cidade);
+    fscanf(fp, "%d", semente);
+
+    fclose(fp);
+    #endif
+}
+
+
+unsigned *gerar_notas(unsigned total_notas)
+{
+    unsigned *notas = (unsigned*) malloc(total_notas * sizeof(unsigned *));
+
+    for (unsigned i = 0; i < total_notas; i++)
+    {
+        #ifdef DEBUG
+        notas[i] = teste[i];
+        #else
+        notas[i] = rand() % NOTAS_POSSIVEIS;
+        #endif
+    }
+
+    return notas;
+}
+
+double obter_menor_valor_double(double *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    double menor_media = NOTAS_POSSIVEIS;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (notas[i] < menor_media)
+            menor_media = notas[i];
+    }
+
+    return menor_media;
+}
+
+double obter_maior_valor_double(double *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    double maior_media = NOTAS_POSSIVEIS;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (notas[i] < maior_media)
+            maior_media = notas[i];
+    }
+
+    return maior_media;
+}
+
+unsigned obter_menor_valor_int(unsigned *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned menor_nota = NOTAS_POSSIVEIS;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (notas[i] < menor_nota)
+            menor_nota = notas[i];
+    }
+
+    return menor_nota;
+}
+
+unsigned obter_maior_valor_int(unsigned *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned maior_nota = 0;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (notas[i] > maior_nota)
+            maior_nota = notas[i];
+    }
+
+    return maior_nota;
+}
+
+unsigned obter_menor_valor_long(unsigned long *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned menor_nota = NOTAS_POSSIVEIS;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (notas[i] < menor_nota)
+            menor_nota = notas[i];
+    }
+
+    return menor_nota;
+}
+
+unsigned obter_maior_valor_long(unsigned long *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned maior_nota = 0;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (notas[i] > maior_nota)
+            maior_nota = notas[i];
+    }
+
+    return maior_nota;
+}
+
+void obter_menor_e_maior_nota(unsigned *notas, unsigned *menor_nota, unsigned *maior_nota, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned maior_nota_tmp = 0;
+    unsigned menor_nota_tmp = NOTAS_POSSIVEIS;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (notas[i] > maior_nota_tmp){
+            maior_nota_tmp = notas[i];
+        }
+        if (notas[i] < menor_nota_tmp){
+            menor_nota_tmp = notas[i];
+        }
+    }  
+    *maior_nota = maior_nota_tmp;
+    *menor_nota = menor_nota_tmp;
+}
+
+unsigned obter_melhor_regiao(unsigned long *soma_de_cada_regiao, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned regiao = 0;
+    unsigned maior_soma = 0;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+    {
+        if (soma_de_cada_regiao[i] > maior_soma){
+            regiao = i;
+            maior_soma = soma_de_cada_regiao[i];
+        }
+    }
+    return regiao;
+}
+void melhor_cidade_e_sua_regiao(unsigned long *soma_de_cada_cidade, unsigned total_regioes, unsigned cidades_por_regiao ,unsigned *melhor_cidade, unsigned*regiao)
+{
+    *melhor_cidade = 0;
+    *regiao = 0;
+
+    unsigned long maior_soma = 0;
+    unsigned last = total_regioes * cidades_por_regiao-1;
+
+    for (unsigned i = 0; i <= last; i++)
+    {
+        if (soma_de_cada_cidade[i] > maior_soma){
+            *melhor_cidade = i % cidades_por_regiao;
+            maior_soma = soma_de_cada_cidade[i];
+            *regiao = i / cidades_por_regiao;
+        }
+    }
+}
+
+unsigned soma_vetor_int(unsigned *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned soma = 0;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+        soma += notas[i];
+
+    return soma;
+}
+
+unsigned long soma_vetor_long(unsigned long *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned long soma = 0;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+        soma += notas[i];
+
+    return soma;
+}
+
+double soma_vetor_double(double *medias, unsigned idx_inicio, unsigned idx_fim)
+{
+    double soma = 0;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+        soma += medias[i];
+
+    return soma;
+}
+
+
+double calcular_soma_para_dp(unsigned *notas, double media, unsigned idx_inicio, unsigned idx_fim)
+{
+    double soma = 0;
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+        soma += pow(notas[i] - media, 2);
+
+    return soma;
+}
+
+
+unsigned *construir_contagem(unsigned *notas, unsigned idx_inicio, unsigned idx_fim)
+{
+    unsigned *contagem = (unsigned*) calloc(NOTAS_POSSIVEIS, sizeof(unsigned));
+
+    for (unsigned i = idx_inicio; i <= idx_fim; i++)
+        contagem[notas[i]]++;
+    
+    return contagem;
+}
+
+double calcular_mediana(unsigned *contagem, unsigned total)
+{
+    unsigned  qtd_contados = 0, metade = total / 2;
+
+    if (total % 2 != 0)
+    {
+        for (unsigned i = 0; i < NOTAS_POSSIVEIS; i++)
+        {
+            for (unsigned j = contagem[i]; j > 0; j--)
+            {
+                if (qtd_contados == metade)
+                    return i;
+                qtd_contados++;
+            }
+        }
+    }
+    else
+    {
+        unsigned meio1, meio2;
+        for (unsigned i = 0; i < NOTAS_POSSIVEIS; i++)
+        {
+            for (unsigned j = contagem[i]; j > 0; j--)
+            {
+                qtd_contados++;
+                if (qtd_contados == metade)
+                {
+                    meio1 = i;
+                    if (j > 1)
+                        return meio1;
+                    i++;
+                    while (!contagem[i]) i++;
+                    meio2 = i;
+                    return (double) (meio1 + meio2) / 2;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+#ifdef DEBUG
+void imprimir_matriz(unsigned *notas, unsigned regiao, unsigned cidades_por_regiao, unsigned alunos_por_cidade)
+{
+    unsigned idx_inicio = regiao * cidades_por_regiao * alunos_por_cidade;
+
+    printf("-> Região %d:\n", regiao);
+    for (unsigned i = 0; i < cidades_por_regiao; i++)
+    {
+        for (unsigned j = 0; j < alunos_por_cidade; j++)
+            printf("%3d  ", notas[idx_inicio + i * alunos_por_cidade + j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+#endif
---
+++ codigo-sequencial/studentsseq.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+#include "funcoes-seq.h"
+
+int main(void)
+{
+    // Leitura dos parâmetros de entrada de arquivo de texto:
+    unsigned total_regioes, cidades_por_regiao, alunos_por_cidade, semente;
+    atribuir_parametros_entrada(&total_regioes, &cidades_por_regiao, &alunos_por_cidade, &semente);
+
+    // Cálculo do total final de notas:
+    unsigned total_final_notas = total_regioes * cidades_por_regiao * alunos_por_cidade;
+    
+    // Geração das notas aleatórias de acordo com a semente lida:
+    srand(semente);
+    unsigned *notas = gerar_notas(total_final_notas);
+
+    // Impressão das matrizes de notas:
+    #ifdef DEBUG
+    printf("\n");
+    for (unsigned regiao = 0; regiao < total_regioes; regiao++)
+        imprimir_matriz(notas, regiao, cidades_por_regiao, alunos_por_cidade);
+    #endif
+
+    // Medição do tempo de resposta:
+    #ifdef RESPONSE_TIME_TESTING 
+    double start, end;
+    start = omp_get_wtime();
+    #endif
+    
+    // Cálculos sequenciais por cidade:
+    unsigned *maior_nota_de_cada_cidade = (unsigned*) malloc(sizeof(unsigned) * cidades_por_regiao * total_regioes);
+    unsigned *menor_nota_de_cada_cidade = (unsigned*) malloc(sizeof(unsigned) * cidades_por_regiao * total_regioes);
+    unsigned long*soma_de_cada_cidade = (unsigned long*) malloc(sizeof(unsigned long) * cidades_por_regiao * total_regioes);
+
+    for (unsigned regiao = 0; regiao < total_regioes; regiao++)
+    {
+        for (unsigned cidade = 0; cidade < cidades_por_regiao; cidade++)
+        {
+            unsigned idx_inicio_cid = regiao * cidades_por_regiao * alunos_por_cidade + cidade * alunos_por_cidade;
+            unsigned idx_fim_cid = regiao * cidades_por_regiao * alunos_por_cidade + cidade * alunos_por_cidade + alunos_por_cidade - 1;
+            unsigned menor_nota_cidade;
+            unsigned maior_nota_cidade;
+            obter_menor_e_maior_nota(notas, &menor_nota_cidade, &maior_nota_cidade, idx_inicio_cid, idx_fim_cid);
+            unsigned long soma_cidade = (unsigned long) soma_vetor_int(notas, idx_inicio_cid, idx_fim_cid);
+            double media_cidade = (double) soma_cidade / (double) alunos_por_cidade; 
+            double dp_cidade = sqrt(calcular_soma_para_dp(notas, media_cidade, idx_inicio_cid, idx_fim_cid) / (alunos_por_cidade - 1)); 
+            unsigned *contagem_cidade = construir_contagem(notas, idx_inicio_cid, idx_fim_cid);
+            double mediana_cidade = calcular_mediana(contagem_cidade, alunos_por_cidade);
+            maior_nota_de_cada_cidade[regiao*cidades_por_regiao + cidade] = maior_nota_cidade;
+            menor_nota_de_cada_cidade[regiao*cidades_por_regiao + cidade] = menor_nota_cidade;
+            soma_de_cada_cidade[regiao*cidades_por_regiao + cidade] = soma_cidade;
+
+            #ifndef RESPONSE_TIME_TESTING
+            printf("Reg %d - Cid %d: menor: %d, maior %d, mediana: %.2lf, média: %.2lf e DP: %.2lf\n",
+                    regiao, cidade, menor_nota_cidade, maior_nota_cidade, mediana_cidade, media_cidade, dp_cidade);
+            #endif
+            free(contagem_cidade);
+        }
+    }
+
+    // Cálculos sequenciais por região:
+
+    unsigned *maior_nota_de_cada_regiao = (unsigned*) malloc(sizeof(unsigned) * total_regioes);
+    unsigned *menor_nota_de_cada_regiao = (unsigned*) malloc(sizeof(unsigned) * total_regioes);
+    unsigned long *soma_de_cada_regiao = (unsigned long*) malloc(sizeof(unsigned long) * total_regioes);
+
+    #ifndef RESPONSE_TIME_TESTING
+    printf("\n");
+    #endif
+    for (unsigned regiao = 0; regiao < total_regioes; regiao++)
+    {
+        unsigned alunos_por_regiao = cidades_por_regiao * alunos_por_cidade;
+        unsigned idx_inicio_reg = regiao * alunos_por_regiao;
+        unsigned idx_fim_reg = regiao * alunos_por_regiao + alunos_por_regiao - 1;
+        unsigned idx_inicio_vetor_cidade = regiao * cidades_por_regiao;
+        unsigned idx_fim_vetor_cidade = idx_inicio_vetor_cidade + cidades_por_regiao-1; 
+        unsigned menor_nota_regiao = obter_menor_valor_int(menor_nota_de_cada_cidade, idx_inicio_vetor_cidade, idx_fim_vetor_cidade);
+        unsigned maior_nota_regiao = obter_maior_valor_int(maior_nota_de_cada_cidade, idx_inicio_vetor_cidade, idx_fim_vetor_cidade);
+        unsigned long soma_regiao = soma_vetor_long(soma_de_cada_cidade, idx_inicio_vetor_cidade, idx_fim_vetor_cidade);
+        double media_regiao =  soma_regiao / (double) (alunos_por_regiao);
+        double dp_regiao = sqrt(calcular_soma_para_dp(notas, media_regiao, idx_inicio_reg, idx_fim_reg) / (alunos_por_cidade * cidades_por_regiao - 1)); 
+        unsigned *contagem_regiao = construir_contagem(notas, idx_inicio_reg, idx_fim_reg);
+        double mediana_regiao = calcular_mediana(contagem_regiao, alunos_por_cidade * cidades_por_regiao);
+        maior_nota_de_cada_regiao[regiao] = maior_nota_regiao;
+        menor_nota_de_cada_regiao[regiao] = menor_nota_regiao;
+        soma_de_cada_regiao[regiao] = soma_regiao;
+        #ifndef RESPONSE_TIME_TESTING
+        printf("Reg %d: menor: %d, maior %d, mediana: %.2lf, média: %.2lf e DP: %.2lf\n",
+                regiao, menor_nota_regiao, maior_nota_regiao, mediana_regiao, media_regiao, dp_regiao);
+        #endif
+        free(contagem_regiao);
+    }
+    // Cálculo sequencial para o Brasil:
+    unsigned menor_nota_brasil = obter_menor_valor_int(menor_nota_de_cada_regiao, 0, total_regioes-1);
+    unsigned maior_nota_brasil = obter_maior_valor_int(maior_nota_de_cada_regiao, 0, total_regioes-1);
+    unsigned long soma_brasil = soma_vetor_long(soma_de_cada_regiao, 0,total_regioes-1);
+    double media_brasil =  (double) soma_brasil / total_final_notas;
+    double dp_brasil = sqrt(calcular_soma_para_dp(notas, media_brasil, 0, total_final_notas - 1) / (total_final_notas - 1)); 
+    unsigned *contagem_brasil = construir_contagem(notas, 0, total_final_notas - 1);
+    double mediana_brasil = calcular_mediana(contagem_brasil, total_final_notas);
+    #ifndef RESPONSE_TIME_TESTING
+    printf("\nBrasil: menor: %d, maior %d, mediana: %.2lf, média: %.2lf e DP: %.2lf\n",
+            menor_nota_brasil, maior_nota_brasil, mediana_brasil, media_brasil, dp_brasil);
+
+    #endif
+    free(contagem_brasil);
+
+    unsigned melhor_regiao = obter_melhor_regiao(soma_de_cada_regiao, 0, total_regioes-1);
+    unsigned regiao_melhor_cidade;
+    unsigned melhor_cidade;
+    melhor_cidade_e_sua_regiao(soma_de_cada_cidade, total_regioes, cidades_por_regiao, &melhor_cidade, &regiao_melhor_cidade);
+
+    free(soma_de_cada_cidade);
+    free(soma_de_cada_regiao);
+    free(menor_nota_de_cada_cidade);
+    free(menor_nota_de_cada_regiao);
+    free(maior_nota_de_cada_cidade);
+    free(maior_nota_de_cada_regiao);
+
+    #ifdef RESPONSE_TIME_TESTING
+    end = omp_get_wtime();
+    printf("Tempo de resposta = %lf\n", end-start);
+    #else
+    printf("Melhor região: Região %d\n", melhor_regiao);
+    printf("Melhor cidade: Região %d, Cidade %d\n", regiao_melhor_cidade, melhor_cidade);
+    #endif
+
+    free(notas);
+
+    return 0;
+}
