be5fa9ba8424e253d7b0c14cb64586f4e8f90c48
Rafael-Corona <rafaelcorona2@gmail.com>
2022-10-26 16:57:12 -0300

    variavel global para evitar que variaveis não printadas não sejam calculadas

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-FLAGS = -Wall -Wextra -lm -fopenmp
+FLAGS = -Wall -Wextra -lm -fopenmp -Wunused -pedantic -ansi -std=c11
 SEQUENCIAL = studentsseq
 PARALELO = studentspar
 SEQUENCIAL_FOLDER = codigo-sequencial
@@ -14,7 +14,7 @@ debug:
 	$(CC) -o $(PARALELO) ./$(PARALLEL_FOLDER)/$(PARALELO).c $(FLAGS) -DDEBUG
 
 time:
-	$(CC) -o $(SEQUENCIAL) ./$(SEQUENCIAL_FOLDER)/$(SEQUENCIAL).c $(FLAGS) -DRESPONSE_TIME_TESTING -Wno-unused
+	$(CC) -o $(SEQUENCIAL) ./$(SEQUENCIAL_FOLDER)/$(SEQUENCIAL).c $(FLAGS) -DRESPONSE_TIME_TESTING 
 	$(CC) -o $(PARALELO) ./$(PARALLEL_FOLDER)/$(PARALELO).c $(FLAGS) -DRESPONSE_TIME_TESTING -Wno-unused
 
 sequencial:
--- codigo-sequencial/studentsseq.c
+++ codigo-sequencial/studentsseq.c
@@ -3,6 +3,11 @@
 #include <math.h>
 #include <omp.h>
 #include "funcoes-seq.h"
+#include <string.h>
+
+double keep_valueD[6]; // buffers para garantir que certas variaveis sejam calculadas
+unsigned keep_valueU[3]; //já que no modo de afericao de tempo, elas não seriam utilizadas
+                        // ou seja, para enganar o compilador
 
 int main(void)
 {
@@ -30,6 +35,21 @@ int main(void)
     unsigned NRUNS = 10;
     double sum = 0;
     double *iterations = malloc(sizeof(double) * NRUNS);
+
+    char filename[255] = "seq";
+    char runs[10], r[15], c[15], a[15];
+    sprintf(runs, "%d", NRUNS);
+    sprintf(r, "%d", total_regioes);
+    sprintf(c, "%d", cidades_por_regiao);
+    sprintf(a, "%d", alunos_por_cidade);
+    strcat(filename, runs);
+    strcat(filename, "exec-RCA");
+    strcat(filename, r);
+    strcat(filename, "-");
+    strcat(filename, c);
+    strcat(filename, "-");
+    strcat(filename, a);    
+    FILE *output_fp = fopen(filename, "w");
 
     for (unsigned i = 0; i < NRUNS; i++)
     {
@@ -63,7 +83,9 @@ int main(void)
             maior_nota_de_cada_cidade[idx_cidade_dentro_brasil] = maior_nota_cidade;
             menor_nota_de_cada_cidade[idx_cidade_dentro_brasil] = menor_nota_cidade;
             soma_de_cada_cidade[idx_cidade_dentro_brasil] = soma_cidade;
-
+            
+            keep_valueD[0] = mediana_cidade;
+            keep_valueD[1] = dp_cidade;
             #ifndef RESPONSE_TIME_TESTING
             printf("Reg %d - Cid %d: menor: %d, maior %d, mediana: %.2lf, média: %.2lf e DP: %.2lf\n",
                     regiao, cidade, menor_nota_cidade, maior_nota_cidade, mediana_cidade, media_cidade, dp_cidade);
@@ -102,6 +124,11 @@ int main(void)
         maior_nota_de_cada_regiao[regiao] = maior_nota_regiao;
         menor_nota_de_cada_regiao[regiao] = menor_nota_regiao;
         soma_de_cada_regiao[regiao] = soma_regiao;
+
+
+        keep_valueD[2] = mediana_regiao;
+        keep_valueD[3] = dp_regiao;
+        
         #ifndef RESPONSE_TIME_TESTING
         printf("Reg %d: menor: %d, maior %d, mediana: %.2lf, média: %.2lf e DP: %.2lf\n",
                 regiao, menor_nota_regiao, maior_nota_regiao, mediana_regiao, media_regiao, dp_regiao);
@@ -115,6 +142,8 @@ int main(void)
     // Cálculo sequencial para o Brasil:
     unsigned menor_nota_brasil = obter_menor_valor_int(menor_nota_de_cada_regiao, 0, total_regioes-1);
     unsigned maior_nota_brasil = obter_maior_valor_int(maior_nota_de_cada_regiao, 0, total_regioes-1);
+    keep_valueU[0] = maior_nota_brasil;
+    keep_valueU[1] = menor_nota_brasil;
     free(maior_nota_de_cada_regiao);
     free(menor_nota_de_cada_regiao);
     
@@ -123,9 +152,14 @@ int main(void)
     double dp_brasil = sqrt(calcular_soma_para_dp(notas, media_brasil, 0, total_final_notas - 1) / (total_final_notas - 1)); 
     unsigned *contagem_brasil = somar_contagens(contagem_de_cada_regiao, 0, total_regioes-1);
 
+    keep_valueD[4] = dp_brasil;
+
     free_lista_contagem(contagem_de_cada_regiao, total_regioes);
 
     double mediana_brasil = calcular_mediana(contagem_brasil, total_final_notas);
+
+    keep_valueD[5] = mediana_brasil;
+
     #ifndef RESPONSE_TIME_TESTING
     printf("\nBrasil: menor: %d, maior %d, mediana: %.2lf, média: %.2lf e DP: %.2lf\n",
             menor_nota_brasil, maior_nota_brasil, mediana_brasil, media_brasil, dp_brasil);
@@ -136,6 +170,7 @@ int main(void)
     unsigned melhor_regiao = obter_melhor_regiao(soma_de_cada_regiao, 0, total_regioes-1);
     unsigned regiao_melhor_cidade;
     unsigned melhor_cidade;
+    keep_valueU[2] = melhor_regiao;
     melhor_cidade_e_sua_regiao(soma_de_cada_cidade, total_regioes, cidades_por_regiao, &melhor_cidade, &regiao_melhor_cidade);
 
     free(soma_de_cada_cidade);
@@ -144,7 +179,7 @@ int main(void)
 
     #ifdef RESPONSE_TIME_TESTING
         end = omp_get_wtime();
-        printf("Iteracao %u. Tempo de resposta = %lf\n",i , end-start);
+        fprintf(output_fp,"Iteracao %u. Tempo de resposta = %lf\n",i , end-start);
         sum += end-start;
         iterations[i] = end-start;
         clean_cache();
@@ -154,14 +189,14 @@ int main(void)
     for (unsigned j = 0; j < NRUNS; j++)
         parcial += pow(iterations[j] - average, 2);
 
-    printf("Tempo médio = %lf\n", average);
-    printf("Desvio padrão = %lf\n", parcial/NRUNS);
+    fprintf(output_fp,"Tempo médio = %lf\n", average);
+    fprintf(output_fp,"Desvio padrão = %lf\n", parcial/NRUNS);
 
     double z_value = 2.576; //correspondente ao intervalo de confiança de 99%
     double error_margin = fabs(z_value * ((parcial/NRUNS) / sqrt(NRUNS)));
 
-    printf("Levando em conta um nível de confiança de 99%% a margem de erro é de: +-%lf", error_margin);
-
+    fprintf(output_fp,"Levando em conta um nível de confiança de 99%% a margem de erro é de: +-%lf", error_margin);
+   
     #else
     printf("Melhor região: Região %d\n", melhor_regiao);
     printf("Melhor cidade: Região %d, Cidade %d\n", regiao_melhor_cidade, melhor_cidade);
